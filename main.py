75323d88e599454f2ce8cd2f4fc1e3520f1d5c39
danielleannamahmud-coder <danielleannamahmud@gmail.com>
2025-12-04 16:43:37 +0000

    Initial comment

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ print (4)
 print ("Hello Danielle, welcome to Codingal class", 4)
 
 print ("\n Hello Danielle, welcome to Codingal class")
+
+print ("The result of 3 + 5 is", 3 + 5)
+
+# This is comment
+
+import keyword
+print (keyword.kwlist)
